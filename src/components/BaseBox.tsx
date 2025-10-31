a9b713973b52da756f288e1791b49c59ce6a8ab6
HuGang <yetalex.hu@gmail.com>
2025-10-31 16:24:20 +0800

    fix: 完成three.js文档【基础】

--- src/components/BaseBox.tsx
+++ src/components/BaseBox.tsx
@@ -16,14 +16,14 @@ const BaseBox = () => {
     canvas.width = width;
     canvas.height = height;
 
-    // 创建渲染器
+    // 1. 创建渲染器
     const renderer = new THREE.WebGLRenderer({ 
       antialias: true, 
       canvas: canvas 
     });
     renderer.setSize(width, height);
 
-    // 需要一个透视摄像机
+    // 2. 需要一个透视摄像机
     const fov = 75;  // 视野范围，这里指垂直方向为75度
     const aspect = width / height; // 画布的宽高比
     const near = 0.1; // 近裁剪面
@@ -32,23 +32,62 @@ const BaseBox = () => {
 
     camera.position.z = 2; // 相机位置在z轴上为2，这样才能看到场景中的物体
 
-    // 需要一个场景
+    // 3. 需要一个场景
     const scene = new THREE.Scene();
     
-    // 需要一个几何体
+    // 4. 需要一个几何体
     const boxWidth = 1;
     const boxHeight = 1;
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
     
-    // 需要一个材质
-    const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+    // 5. 需要一个材质
+    // const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
     
-    // 需要一个网格
-    const mesh = new THREE.Mesh(geometry, material);
+    // 6. 需要一个网格
+    // const cube = new THREE.Mesh(geometry, material);
     
     // 把网格添加到场景中
-    scene.add(mesh);
+    // scene.add(cube);
+
+    function makeInstance(geometry: THREE.BufferGeometry, color: number, x: number) {
+      const material = new THREE.MeshPhongMaterial({ color });
+      const cube = new THREE.Mesh(geometry, material);
+      cube.position.x = x;
+      scene.add(cube);
+      return cube;
+    }
+    const cubes = [
+      makeInstance(geometry, 0x44aa88,  0),
+      makeInstance(geometry, 0x8844aa, -2),
+      makeInstance(geometry, 0xaa8844,  2),
+    ];
+
+    function render(time: number) {
+      time *= 0.001; // 转换为秒
+
+      cubes.forEach((cube, ndx) => {
+        const speed = 1 + ndx * .1;
+        const rot = time * speed;
+        cube.rotation.x = rot;
+        cube.rotation.y = rot;
+      })
+
+      // cube.rotation.x = time;
+      // cube.rotation.y = time;
+
+      renderer.render(scene, camera);
+
+      requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
+    
+    // 7. 创建一束光
+    const color = 0xFFFFFF;
+    const intnsity = 3;
+    const light = new THREE.DirectionalLight(color, intnsity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
 
     // 渲染场景
     renderer.render(scene, camera);
@@ -56,7 +95,7 @@ const BaseBox = () => {
     // 清理函数
     return () => {
       geometry.dispose();
-      material.dispose();
+      // material.dispose();
       renderer.dispose();
     };
   }, []);
